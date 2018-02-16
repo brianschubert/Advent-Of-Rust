30731185e1dd5286ebfd79e9adb61c9ec5c6284b
brian <brianm.schubert@gmail.com>
2018-02-16 17:07:38 -0500

    Add solution for 2015 day 04

---
+++ src/y2015/day04/mod.rs
@@ -0,0 +1,68 @@
+//! Solution for 2015 Day 04
+
+use common::puzzle::{input as pio, PuzzleSelection as Pz, Solution, PuzzleResult};
+use crypto::md5::Md5;
+use crypto::digest::Digest;
+
+pub fn solve(puzzle: &Pz) -> PuzzleResult {
+    let input = pio::fetch_string(puzzle)?;
+
+    solve_parts! {
+        both => find_first_coin(input.as_bytes())
+    }
+}
+
+/// Returns first "AdventCoins" generated during part one and part two,
+/// respectively,
+fn find_first_coin(seed: &[u8]) -> (u64, u64) {
+    let mut hasher = Md5::new();
+    let mut coin_5 = None;
+    let mut coin_6 = None;
+
+    for index in 0..u64::max_value() {
+        hasher.input(seed);
+        hasher.input(index.to_string().as_bytes());
+
+        let mut result = [0_u8; 16];
+        hasher.result(&mut result);
+
+        // Check if first 5 chars in hex representation would be 0
+        if result[0] as u16 + result[1] as u16 + (result[2] >> 4) as u16 == 0 {
+            if coin_5.is_none() {
+                coin_5 = Some(index);
+            }
+            // Check if the sixth is also a 0
+            if result[2] == 0 && coin_6.is_none() {
+                coin_6 = Some(index);
+            }
+        }
+
+        if let (Some(one), Some(two)) = (coin_5, coin_6) {
+            return (one, two);
+        }
+
+        hasher.reset();
+    }
+    unreachable!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn solution() {
+        assert_solution!(
+            254575,
+            1038736,
+            Pz::new(2015, 4)
+        )
+    }
+
+    #[test]
+    fn ex1() {
+        // Part two causes these to run for ~60s
+        assert_eq!(609043, find_first_coin(b"abcdef").0);
+        assert_eq!(1048970, find_first_coin(b"pqrstuv").0);
+    }
+}
--- src/y2015/mod.rs
+++ src/y2015/mod.rs
@@ -1,10 +1,12 @@
 pub mod day01;
 pub mod day02;
 pub mod day03;
+pub mod day04;
 
 route_days! {
     1 => day01,
     2 => day02,
-    3 => day03
+    3 => day03,
+    4 => day04
 
 }

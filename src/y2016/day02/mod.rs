e0cf55c7bb408cc19b935fc841d01527e14b0e15
brian <brianm.schubert@gmail.com>
2018-02-21 20:59:50 -0500

    Improve documentation and error handling for 2016 day 02

--- src/y2016/day02/mod.rs
+++ src/y2016/day02/mod.rs
@@ -3,14 +3,17 @@
 use common::puzzle::{input as pio, PuzzleSelection as Pz, Solution, PuzzleResult};
 use common::util::Pt;
 
+/// The keypad used to determine the passcode during part one.
 const KEYPAD_ONE: [&'static [char]; 3] = [
     &['1', '2', '3'],
     &['4', '5', '6'],
     &['7', '8', '9'],
 ];
 
+/// The "finger" starting position on the part one keypad.
 const START_ONE: Pt<i8> = Pt { x: 1, y: 1 };
 
+/// The keypad used to determine the passcode during part two.
 const KEYPAD_TWO: [&'static [char]; 5] = [
     &['x', 'x', '1', 'x', 'x'],
     &['x', '2', '3', '4', 'x'],
@@ -19,30 +22,39 @@ const KEYPAD_TWO: [&'static [char]; 5] = [
     &['x', 'x', 'D', 'x', 'x'],
 ];
 
+/// The "finger" starting position on the part two keypad.
 const START_TWO: Pt<i8> = Pt { x: 0, y: 2 };
 
 pub fn solve(puzzle: &Pz) -> PuzzleResult {
     let input = pio::fetch_lines(puzzle)?;
 
     solve_parts! {
-        1 => press_keycode(&KEYPAD_ONE, &input, START_ONE),
-        2 => press_keycode(&KEYPAD_TWO, &input, START_TWO)
+        1 => press_keycode(&KEYPAD_ONE, &input, START_ONE)?,
+        2 => press_keycode(&KEYPAD_TWO, &input, START_TWO)?
     }
 }
 
-fn press_keycode(keypad: &[&[char]], instr: &[String], start: Pt<i8>) -> String {
+/// Returns the passcode resulting from following the specified
+/// instructions of the specified keypad.
+///
+/// An error is returned if an invalid instruction is found.
+fn press_keycode<S: AsRef<str>>(
+    keypad: &[&[char]],
+    instr: &[S],
+    start: Pt<i8>,
+) -> Result<String, String> {
     // keypad assumed to be a square
     let dim = keypad.len() - 1;
     let mut finger = start.clone();
 
     instr.iter().map(|line| {
-        for byte in line.as_bytes() {
+        for byte in line.as_ref().as_bytes() {
             let next = finger + match *byte {
                 b'U' => Pt::<i8>::n(),
                 b'R' => Pt::e(),
                 b'D' => Pt::s(),
                 b'L' => Pt::w(),
-                _ => panic!(format!("Bad direction: {}", &byte))
+                b => Err(format!("Bad direction: {}", b as char))?
             };
             {
                 let Pt { x, y } = next;
@@ -53,7 +65,7 @@ fn press_keycode(keypad: &[&[char]], instr: &[String], start: Pt<i8>) -> String 
                 }
             }
         }
-        keypad[dim - finger.y as usize][finger.x as usize]
+        Ok(keypad[dim - finger.y as usize][finger.x as usize])
     }).collect()
 }
 
@@ -73,25 +85,31 @@ mod tests {
     #[test]
     fn ex1() {
         let instr = [
-            "ULL".to_owned(),
-            "RRDDD".to_owned(),
-            "LURDL".to_owned(),
-            "UUUUD".to_owned()
+            "ULL",
+            "RRDDD",
+            "LURDL",
+            "UUUUD"
         ];
 
-        assert_eq!("1985", press_keycode(&KEYPAD_ONE, &instr, START_TWO));
+        assert_eq!(
+            "1985",
+            press_keycode(&KEYPAD_ONE, &instr, START_TWO).unwrap()
+        );
     }
 
     #[test]
     fn ex2() {
         let instr = [
-            "ULL".to_owned(),
-            "RRDDD".to_owned(),
-            "LURDL".to_owned(),
-            "UUUUD".to_owned()
+            "ULL",
+            "RRDDD",
+            "LURDL",
+            "UUUUD"
         ];
 
-        assert_eq!("5DB3", press_keycode(&KEYPAD_TWO, &instr, START_TWO));
+        assert_eq!(
+            "5DB3",
+            press_keycode(&KEYPAD_TWO, &instr, START_TWO).unwrap()
+        );
     }
 }
 

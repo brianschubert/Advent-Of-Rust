be30e42f1443752a9c985f9e4f070ee856afa94a
brian <brianm.schubert@gmail.com>
2018-02-26 18:27:05 -0500

    Optimize solution for 2016 day 01
    Improves documentation, error handling, and memory efficiency of the
    puzzle's solution

--- src/y2016/day01/mod.rs
+++ src/y2016/day01/mod.rs
@@ -8,33 +8,35 @@ pub fn solve(puzzle: &Pz) -> PuzzleResult {
 
     solve_parts! {
         both => {
-            let (end, intersect) = walk_blocks(input.trim_right());
+            let (end, intersect) = walk_blocks(input.trim_right())?;
             (end, intersect.ok_or("Instructions never intersect")?)
         }
     }
 }
 
-fn walk_blocks(instr: &str) -> (i16, Option<i16>) {
-    let instructions: Vec<String> = instr
-        .split(", ")
-        .map(|s: &str| s.to_owned())
-        .collect();
-
+/// Attempts to perform the "walk" described by the specified string.
+///
+/// Returns a result tuple containing (0) the final distance from the origin
+/// after performing the walk and (1) an optional distance to the first point
+/// of intersection that occurred during the walk.
+///
+/// If the specified walk is malformed, an error string is returned.
+fn walk_blocks(instr: &str) -> Result<(i16, Option<i16>), &'static str> {
     let mut pos = Pt::origin();
     let mut dir: Pt<i8> = Pt::n();
 
     let mut previous = Vec::new();
     let mut intersect: Option<Pt<i16>> = None;
 
-    for instr in instructions.iter() {
+    for instr in instr.split(", ") {
         let (turn, mag) = instr.split_at(1);
         dir = match turn {
             "R" => dir.rot90r(),
             "L" => dir.rot90l(),
-            _ => panic!("Bad turn token")
+            _ => Err("Malformed turn direction")?
         };
 
-        for _ in 0..mag.parse().unwrap() {
+        for _ in 0..mag.parse().map_err(|_| "malformed move magnitude")? {
             pos += dir;
             if intersect.is_none() {
                 if previous.contains(&pos) {
@@ -46,10 +48,10 @@ fn walk_blocks(instr: &str) -> (i16, Option<i16>) {
         }
     }
 
-    (
+    Ok((
         pos.dist_manh(&Pt::origin()),
         intersect.map(|p| p.dist_manh(&Pt::origin()))
-    )
+    ))
 }
 
 #[cfg(test)]
@@ -67,13 +69,13 @@ mod tests {
 
     #[test]
     fn ex1() {
-        assert_eq!(5, walk_blocks("R2, L3").0);
-        assert_eq!(2, walk_blocks("R2, R2, R2").0);
-        assert_eq!(12, walk_blocks("R5, L5, R5, R3").0);
+        assert_eq!(5, walk_blocks("R2, L3").unwrap().0);
+        assert_eq!(2, walk_blocks("R2, R2, R2").unwrap().0);
+        assert_eq!(12, walk_blocks("R5, L5, R5, R3").unwrap().0);
     }
 
     #[test]
     fn ex2() {
-        assert_eq!(4, walk_blocks("R8, R4, R4, R8").1.unwrap())
+        assert_eq!(4, walk_blocks("R8, R4, R4, R8").unwrap().1.unwrap())
     }
 }

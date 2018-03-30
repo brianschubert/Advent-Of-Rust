3b82488acd463ae420e5b07d8f6dbf603ff0aea1
brian <brianm.schubert@gmail.com>
2018-03-30 10:44:07 -0400

    Improve error propagation for 2016 Day 12 and 23

--- src/y2016/day12/mod.rs
+++ src/y2016/day12/mod.rs
@@ -5,7 +5,7 @@ use common::puzzle::{input as pio, PuzzleSelection as Pz, Solution, PuzzleResult
 pub mod assembunny;
 
 pub fn solve(puzzle: &Pz) -> PuzzleResult {
-    let input = parse_instructions(&pio::fetch_lines(puzzle)?);
+    let input = parse_instructions(&pio::fetch_lines(puzzle)?)?;
 
     solve_parts! {
         1 => {
@@ -31,10 +31,10 @@ pub fn solve(puzzle: &Pz) -> PuzzleResult {
 /// Parses the specified lines into assembunny instructions.
 ///
 /// Panics on underlying parse errors.
-fn parse_instructions<T>(lines: &[T]) -> Vec<assembunny::Instr>
-    where T: AsRef<str> + ::std::fmt::Debug
+fn parse_instructions<T>(lines: &[T]) -> Result<Vec<assembunny::Instr>, &'static str>
+    where T: AsRef<str>
 {
-    lines.iter().map(|l| l.as_ref().parse().unwrap()).collect()
+    lines.iter().map(|l| l.as_ref().parse()).collect()
 }
 
 #[cfg(test)]
@@ -63,7 +63,7 @@ mod tests {
             "dec a",
         ];
 
-        let assem_instr = parse_instructions(&instr);
+        let assem_instr = parse_instructions(&instr).unwrap();
 
         let mut interp = Interpreter::new(&assem_instr);
 
--- src/y2016/day23/mod.rs
+++ src/y2016/day23/mod.rs
@@ -3,7 +3,7 @@
 //! Part two take a good two-three minute to run due to my reluctance
 //! to optimize out the addition loops in the assembunny input.
 
-use common::puzzle::{input as pio, PuzzleSelection as Pz, Solution, PuzzleResult};
+use common::puzzle::{input as pio, PuzzleResult, PuzzleSelection as Pz, Solution};
 
 /// Initial value of register `a` in part one.
 const EGG_COUNT_ONE: i32 = 7;
@@ -14,7 +14,7 @@ const EGG_COUNT_TWO: i32 = 12;
 pub mod assembunny_toggle;
 
 pub fn solve(puzzle: &Pz) -> PuzzleResult {
-    let input = parse_instructions(&pio::fetch_lines(puzzle)?);
+    let input = parse_instructions(&pio::fetch_lines(puzzle)?)?;
 
     solve_parts! {
         1 => {
@@ -45,10 +45,10 @@ pub fn solve(puzzle: &Pz) -> PuzzleResult {
 /// Parses the specified lines into assembunny instructions.
 ///
 /// Panics on underlying parse errors.
-fn parse_instructions<T>(lines: &[T]) -> Vec<assembunny_toggle::InstrWrapper>
+fn parse_instructions<T>(lines: &[T]) -> Result<Vec<assembunny_toggle::InstrWrapper>, &'static str>
     where T: AsRef<str>
 {
-    lines.iter().map(|l| l.as_ref().parse().unwrap()).collect()
+    lines.iter().map(|l| l.as_ref().parse()).collect()
 }
 
 #[cfg(test)]
@@ -80,7 +80,7 @@ mod tests {
             "dec a",
         ];
 
-        let mut interp = Interpreter::new(parse_instructions(&instr));
+        let mut interp = Interpreter::new(parse_instructions(&instr).unwrap());
 
         while !interp.done() {
             interp.execute_next();

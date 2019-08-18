c7d2109c461d11f57328b79e22a50a5c30232c25
Brian Schubert <brianm.schubert@gmail.com>
2019-08-17 21:45:10 -0400

    Add optional trailing common and required imports to common marcos

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -1,6 +1,5 @@
 pub mod puzzle;
 pub mod util;
-pub mod prelude;
 
 macro_rules! route_days {
     ( $( $day:expr => $sol:ident ),+ ) => {
@@ -25,16 +24,20 @@ macro_rules! bench_ans {
 }
 
 macro_rules! solve_parts {
-    ( 1 => $part_one:expr ) => { Ok(Solution(Some(bench_ans!($part_one)), None)) };
+    ( 1 => $part_one:expr $(,)?) => {{
+        use crate::common::puzzle::Solution;
+        Ok(Solution(Some(bench_ans!($part_one)), None))
+    }};
 
-    ( 1 => $part_one:expr, 2 => $part_two:expr ) => {
+    ( 1 => $part_one:expr, 2 => $part_two:expr $(,)? ) => {{
+        use crate::common::puzzle::Solution;
         Ok(Solution(
             Some(bench_ans!($part_one)),
-            Some(bench_ans!($part_two))
+            Some(bench_ans!($part_two)),
         ))
-    };
+    }};
 
-   ( both => $part_producer:expr ) => {{
+    ( both => $part_producer:expr ) => {{
         use crate::common::puzzle::Answer;
         use std::time::Instant;
 
@@ -44,23 +47,23 @@ macro_rules! solve_parts {
 
         Ok(Solution(
             Some(Answer::with_bench(part_one, Some(bench))),
-            Some(Answer::with_bench(part_two, None))
+            Some(Answer::with_bench(part_two, None)),
         ))
-   }}
+    }};
 }
 
 #[cfg(test)]
 macro_rules! assert_solution {
-    ( $part_one:expr, $puzzle:expr) => {{
-        use crate::common::puzzle::{Solution, Answer};
+    ( $part_one:expr, $puzzle:expr $(,)?) => {{
+        use crate::common::puzzle::{Answer, Solution};
         assert_eq! {
             Solution::new(Some(Answer::new($part_one)), None),
             solve(&$puzzle).unwrap()
         }
     }};
 
-    ( $part_one:expr, $part_two:expr, $puzzle:expr) => {{
-        use crate::common::puzzle::{Solution, Answer};
+    ( $part_one:expr, $part_two:expr, $puzzle:expr $(,)?) => {{
+        use crate::common::puzzle::{Answer, Solution};
         assert_eq! {
             Solution::new(Some(Answer::new($part_one)), Some(Answer::new($part_two))),
             solve(&$puzzle).unwrap()
